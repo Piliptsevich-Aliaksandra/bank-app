1bf911270bfd97109ffc3aaf5e036870b9d8cd6b
Piliptsevich Aliaksandra <sashapiliptsevich@gmail.com>
2025-01-30 00:08:01 +0300

    add test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 @patch("app.main.init_db")
 def test_startup_event(mock_init_db):
-    startup_event()  # Вызываем хук старта вручную
+    startup_event()
     mock_init_db.assert_called_once()
 
 
@@ -15,3 +15,8 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the banking system API!"}
+
+
+def test_metrics():
+    response = client.get("/metrics")
+    assert response.status_code == 200
